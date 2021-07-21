273f48ff7c1495bb9ccb3897c400832b29df7598
wangbin <wangbin@emqx.io>
2021-07-21 17:40:37 +0800

    feat: add github action ci

---
+++ script/prepare-test-env.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+set -e
+
+DEFAULT_HSTREAM_DOCKER_TAG="v0.5.0.0"
+
+docker pull hstreamdb/hstream:${DEFAULT_HSTREAM_DOCKER_TAG} || true 
+
+mkdir /tmp/hstream-data
+
+docker run \
+    -td \
+    --rm  \
+    -v /tmp/hstream-data:/data/store \
+    --network host \
+    hstreamdb/hstream:${DEFAULT_HSTREAM_DOCKER_TAG} \
+    ld-dev-cluster --root /data/store --use-tcp
+
+docker run \
+  -td \
+  --rm \
+  -v /tmp/hstream-data:/data/store \
+  --network host \
+  hstreamdb/hstream:${DEFAULT_HSTREAM_DOCKER_TAG} \
+  hstream-server --port 6570 --store-config /data/store/logdevice.conf
