6f6baf73c1d8603c5c0cebd1100e1ecf62424d6b
wangbin <wangbin@emqx.io>
2021-08-03 16:23:56 +0800

    update commit in proto

--- src/test/java/io/hstream/HStreamClientTest.java
+++ src/test/java/io/hstream/HStreamClientTest.java
@@ -1,7 +1,6 @@
 package io.hstream;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
