6e6acd8ce199ca3f8e57669e440a7dc134bb7e5e
wangbin <wangbin@emqx.io>
2021-08-13 14:50:34 +0800

    add javadoc for Queryer and QueryerBuilder

--- src/main/java/io/hstream/Queryer.java
+++ src/main/java/io/hstream/Queryer.java
@@ -2,4 +2,5 @@ package io.hstream;
 
 import com.google.common.util.concurrent.Service;
 
+/** The interface that abstracts behavior of HStream's queryer */
 public interface Queryer extends Service {}
--- src/main/java/io/hstream/QueryerBuilder.java
+++ src/main/java/io/hstream/QueryerBuilder.java
@@ -2,6 +2,7 @@ package io.hstream;
 
 import io.hstream.impl.QueryerImpl;
 
+/** Builder used to configure and construct a {@link Queryer} instance. */
 public class QueryerBuilder {
 
   private HStreamClient client;
@@ -14,16 +15,33 @@ public class QueryerBuilder {
     this.grpcStub = grpcStub;
   }
 
+  /**
+   * Set SQL statement the queryer will execute.
+   *
+   * @param sql SQL statement, only allow statements like "select ... emit changes"
+   * @return the {@link QueryerBuilder} instance
+   */
   public QueryerBuilder sql(String sql) {
     this.sql = sql;
     return this;
   }
 
+  /**
+   * Set {@link Observer} for sql results.
+   *
+   * @param resultObserver the {@link Observer} instance
+   * @return the {@link QueryerBuilder} instance
+   */
   public QueryerBuilder resultObserver(Observer<HRecord> resultObserver) {
     this.resultObserver = resultObserver;
     return this;
   }
 
+  /**
+   * Construct the final {@link Queryer} instance.
+   *
+   * @return the {@link Queryer} instance
+   */
   public Queryer build() {
     return new QueryerImpl(client, grpcStub, sql, resultObserver);
   }
