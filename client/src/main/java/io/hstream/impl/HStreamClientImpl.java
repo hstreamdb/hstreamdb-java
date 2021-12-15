c6b369b9b8354bec7d5ff801ec48f5d53dc1bb58
alissa-tung <alissa-tung@outlook.com>
2021-12-15 17:16:47 +0800

    retry: refactor

--- client/src/main/java/io/hstream/impl/HStreamClientImpl.java
+++ client/src/main/java/io/hstream/impl/HStreamClientImpl.java
@@ -1,7 +1,6 @@
 package io.hstream.impl;
 
 import com.google.protobuf.Empty;
-import io.grpc.ManagedChannel;
 import io.hstream.ConsumerBuilder;
 import io.hstream.HStreamClient;
 import io.hstream.ProducerBuilder;
@@ -11,13 +10,13 @@ import io.hstream.Subscription;
 import io.hstream.internal.DeleteStreamRequest;
 import io.hstream.internal.DeleteSubscriptionRequest;
 import io.hstream.internal.DescribeClusterResponse;
-import io.hstream.internal.HStreamApiGrpc;
 import io.hstream.internal.ListStreamsResponse;
 import io.hstream.internal.ServerNode;
 import io.hstream.util.GrpcUtils;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,40 +33,20 @@ public class HStreamClientImpl implements HStreamClient {
   public HStreamClientImpl(List<String> bootstrapServerUrls) {
     channelProvider = new ChannelProvider();
 
-    boolean retryStatus = false;
-    List<ServerNode> serverNodes = null;
-    List<String> initializedServerUrls = null;
+    AtomicReference<List<ServerNode>> serverNodes = new AtomicReference<>();
+    List<String> initializedServerUrls;
 
-    for (int retryAcc = 0; retryAcc < bootstrapServerUrls.size() && !retryStatus; retryAcc++) {
-      logger.info("begin describeCluster");
-      try {
-        ManagedChannel channel = channelProvider.get(bootstrapServerUrls.get(retryAcc));
-        HStreamApiGrpc.HStreamApiBlockingStub blockingStub =
-            HStreamApiGrpc.newBlockingStub(channel);
-        DescribeClusterResponse describeClusterResponse =
-            blockingStub.describeCluster(Empty.newBuilder().build());
+    new Retry(bootstrapServerUrls, logger)
+        .withRetriesBlocking(
+            stub -> {
+              DescribeClusterResponse describeClusterResponse =
+                  stub.describeCluster(Empty.newBuilder().build());
+              serverNodes.set(describeClusterResponse.getServerNodesList());
+            });
 
-        serverNodes = describeClusterResponse.getServerNodesList();
-        initializedServerUrls = new ArrayList<>(serverNodes.size());
-        retryStatus = true;
-      } catch (Exception e) {
-        logger.warn(
-            "retry because of "
-                + e
-                + ", "
-                + "bootstrapServerUrl = "
-                + bootstrapServerUrls.get(retryAcc)
-                + " retryAcc = "
-                + retryAcc);
-        if (!(retryAcc + 1 < bootstrapServerUrls.size())) {
-          logger.error("retry failed, " + "retryAcc = " + retryAcc, e);
-          throw e;
-        }
-      }
-    }
-    logger.info("end describeCluster");
+    initializedServerUrls = new ArrayList<>(serverNodes.get().size());
     this.initializedServerUrls = initializedServerUrls;
-    for (ServerNode serverNode : serverNodes) {
+    for (ServerNode serverNode : serverNodes.get()) {
       String host = serverNode.getHost();
       int port = serverNode.getPort();
       initializedServerUrls.add(host + ":" + port);
@@ -98,30 +77,11 @@ public class HStreamClientImpl implements HStreamClient {
   public void createStream(String streamName, short replicationFactor) {
     final Stream stream = new Stream(streamName, replicationFactor);
 
-    boolean retryStatus = false;
-    for (int retryAcc = 0; retryAcc < initializedServerUrls.size() && !retryStatus; retryAcc++) {
-      logger.info("begin createStream");
-      try {
-        HStreamApiGrpc.HStreamApiBlockingStub blockingStub =
-            HStreamApiGrpc.newBlockingStub(
-                channelProvider.get(initializedServerUrls.get(retryAcc)));
-        blockingStub.createStream(GrpcUtils.streamToGrpc(stream));
-      } catch (Exception e) {
-        logger.warn(
-            "retry because of "
-                + e
-                + ", "
-                + "initializedServerUrl = "
-                + initializedServerUrls.get(retryAcc)
-                + " retryAcc = "
-                + retryAcc);
-        if (!(retryAcc + 1 < initializedServerUrls.size())) {
-          logger.error("retry failed, " + "retryAcc = " + retryAcc, e);
-          throw e;
-        }
-      }
-    }
-    logger.info("end createStream");
+    new Retry(initializedServerUrls, logger)
+        .withRetriesBlocking(
+            (stub) -> {
+              stub.createStream(GrpcUtils.streamToGrpc(stream));
+            });
   }
 
   @Override
@@ -129,162 +89,71 @@ public class HStreamClientImpl implements HStreamClient {
     final DeleteStreamRequest deleteStreamRequest =
         DeleteStreamRequest.newBuilder().setStreamName(streamName).build();
 
-    boolean retryStatus = false;
-    for (int retryAcc = 0; retryAcc < initializedServerUrls.size() && !retryStatus; retryAcc++) {
-      logger.info("begin deleteStream");
-      try {
-        HStreamApiGrpc.HStreamApiBlockingStub blockingStub =
-            HStreamApiGrpc.newBlockingStub(
-                channelProvider.get(initializedServerUrls.get(retryAcc)));
-        blockingStub.deleteStream(deleteStreamRequest);
-      } catch (Exception e) {
-        logger.warn(
-            "retry because of "
-                + e
-                + ", "
-                + "initializedServerUrl = "
-                + initializedServerUrls.get(retryAcc)
-                + " retryAcc = "
-                + retryAcc);
-        if (!(retryAcc + 1 < initializedServerUrls.size())) {
-          logger.error("retry failed, " + "retryAcc = " + retryAcc, e);
-          throw e;
-        }
-      }
-    }
+    new Retry(initializedServerUrls, logger)
+        .withRetriesBlocking(
+            stub -> {
+              stub.deleteStream(deleteStreamRequest);
+            });
+
     logger.info("delete stream {} done", streamName);
   }
 
   @Override
   public List<Stream> listStreams() {
     Empty empty = Empty.newBuilder().build();
+    AtomicReference<ListStreamsResponse> listStreamsResponse = new AtomicReference<>();
 
-    ListStreamsResponse listStreamsResponse = null;
+    new Retry(initializedServerUrls, logger)
+        .withRetriesBlocking(
+            stub -> {
+              listStreamsResponse.set(stub.listStreams(empty));
+            });
 
-    boolean retryStatus = false;
-    for (int retryAcc = 0; retryAcc < initializedServerUrls.size() && !retryStatus; retryAcc++) {
-      logger.info("begin listStreams");
-      try {
-        HStreamApiGrpc.HStreamApiBlockingStub blockingStub =
-            HStreamApiGrpc.newBlockingStub(
-                channelProvider.get(initializedServerUrls.get(retryAcc)));
-        listStreamsResponse = blockingStub.listStreams(empty);
-      } catch (Exception e) {
-        logger.warn(
-            "retry because of "
-                + e
-                + ", "
-                + "initializedServerUrl = "
-                + initializedServerUrls.get(retryAcc)
-                + " retryAcc = "
-                + retryAcc);
-        if (!(retryAcc + 1 < initializedServerUrls.size())) {
-          logger.error("retry failed, " + "retryAcc = " + retryAcc, e);
-          throw e;
-        }
-      }
-    }
-    logger.info("end listStreams");
-    return listStreamsResponse.getStreamsList().stream()
+    return listStreamsResponse.get().getStreamsList().stream()
         .map(GrpcUtils::streamFromGrpc)
         .collect(Collectors.toList());
   }
 
   @Override
   public void createSubscription(Subscription subscription) {
-    boolean retryStatus = false;
-    for (int retryAcc = 0; retryAcc < initializedServerUrls.size() && !retryStatus; retryAcc++) {
-      logger.info("begin createSubscription");
-      try {
-        HStreamApiGrpc.HStreamApiBlockingStub blockingStub =
-            HStreamApiGrpc.newBlockingStub(
-                channelProvider.get(initializedServerUrls.get(retryAcc)));
-        blockingStub.createSubscription(GrpcUtils.subscriptionToGrpc(subscription));
-      } catch (Exception e) {
-        logger.warn(
-            "retry because of "
-                + e
-                + ", "
-                + "initializedServerUrl = "
-                + initializedServerUrls.get(retryAcc)
-                + " retryAcc = "
-                + retryAcc);
-        if (!(retryAcc + 1 < initializedServerUrls.size())) {
-          logger.error("retry failed, " + "retryAcc = " + retryAcc, e);
-          throw e;
-        }
-      }
-    }
-    logger.info("end createSubscription");
+    new Retry(initializedServerUrls, logger)
+        .withRetriesBlocking(
+            stub -> {
+              stub.createSubscription(GrpcUtils.subscriptionToGrpc(subscription));
+            });
   }
 
   @Override
   public List<Subscription> listSubscriptions() {
-    List<Subscription> ret = null;
+    AtomicReference<List<Subscription>> listSubscriptionsResponse = new AtomicReference<>();
 
-    boolean retryStatus = false;
-    for (int retryAcc = 0; retryAcc < initializedServerUrls.size() && !retryStatus; retryAcc++) {
-      logger.info("begin listSubscriptions");
-      try {
-        HStreamApiGrpc.HStreamApiBlockingStub blockingStub =
-            HStreamApiGrpc.newBlockingStub(
-                channelProvider.get(initializedServerUrls.get(retryAcc)));
-        ret =
-            blockingStub
-                .withDeadlineAfter(100, TimeUnit.SECONDS)
-                .listSubscriptions(Empty.newBuilder().build())
-                .getSubscriptionList()
-                .stream()
-                .map(GrpcUtils::subscriptionFromGrpc)
-                .collect(Collectors.toList());
-      } catch (Exception e) {
-        logger.warn(
-            "retry because of "
-                + e
-                + ", "
-                + "initializedServerUrl = "
-                + initializedServerUrls.get(retryAcc)
-                + " retryAcc = "
-                + retryAcc);
-        if (!(retryAcc + 1 < initializedServerUrls.size())) {
-          logger.error("retry failed, " + "retryAcc = " + retryAcc, e);
-          throw e;
-        }
-      }
-    }
-    logger.info("end listSubscriptions");
-    return ret;
+    new Retry(initializedServerUrls, logger)
+        .withRetriesBlocking(
+            stub -> {
+              listSubscriptionsResponse.set(
+                  stub
+                      .withDeadlineAfter(100, TimeUnit.SECONDS)
+                      .listSubscriptions(Empty.newBuilder().build())
+                      .getSubscriptionList()
+                      .stream()
+                      .map(GrpcUtils::subscriptionFromGrpc)
+                      .collect(Collectors.toList()));
+            });
+
+    return listSubscriptionsResponse.get();
   }
 
   @Override
   public void deleteSubscription(String subscriptionId) {
-    boolean retryStatus = false;
-    for (int retryAcc = 0; retryAcc < initializedServerUrls.size() && !retryStatus; retryAcc++) {
-      logger.info("begin deleteSubscription");
-      try {
-        HStreamApiGrpc.HStreamApiBlockingStub blockingStub =
-            HStreamApiGrpc.newBlockingStub(
-                channelProvider.get(initializedServerUrls.get(retryAcc)));
-        blockingStub
-            .withDeadlineAfter(100, TimeUnit.SECONDS)
-            .deleteSubscription(
-                DeleteSubscriptionRequest.newBuilder().setSubscriptionId(subscriptionId).build());
-      } catch (Exception e) {
-        logger.warn(
-            "retry because of "
-                + e
-                + ", "
-                + "initializedServerUrl = "
-                + initializedServerUrls.get(retryAcc)
-                + " retryAcc = "
-                + retryAcc);
-        if (!(retryAcc + 1 < initializedServerUrls.size())) {
-          logger.error("retry failed, " + "retryAcc = " + retryAcc, e);
-          throw e;
-        }
-      }
-    }
-    logger.info("delete subscription {} done", subscriptionId);
+    new Retry(initializedServerUrls, logger)
+        .withRetriesBlocking(
+            stub -> {
+              stub.withDeadlineAfter(100, TimeUnit.SECONDS)
+                  .deleteSubscription(
+                      DeleteSubscriptionRequest.newBuilder()
+                          .setSubscriptionId(subscriptionId)
+                          .build());
+            });
   }
 
   @Override
