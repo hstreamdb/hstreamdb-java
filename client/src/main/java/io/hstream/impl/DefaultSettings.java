441b6762a872d568e431293f2cd4c007e1b9b4c2
wangbin <wangbin@emqx.io>
2022-01-04 14:53:37 +0800

    adjust default request retry interval to 5 seconds

--- client/src/main/java/io/hstream/impl/DefaultSettings.java
+++ client/src/main/java/io/hstream/impl/DefaultSettings.java
@@ -2,7 +2,7 @@ package io.hstream.impl;
 
 public class DefaultSettings {
 
-  public static final long REQUEST_RETRY_INTERVAL_SECONDS = 3;
+  public static final long REQUEST_RETRY_INTERVAL_SECONDS = 5;
 
   public static final int APPEND_RETRY_MAX_TIMES = 5;
 }
