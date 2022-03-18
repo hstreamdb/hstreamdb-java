ee8851ece5cf676f09e48575a25299500f865ebf
daleiz <30970925+daleiz@users.noreply.github.com>
2022-03-18 17:42:02 +0800

    add backlogDuration for stream (#87)

--- client/src/main/java/io/hstream/Stream.java
+++ client/src/main/java/io/hstream/Stream.java
@@ -5,43 +5,50 @@ import java.util.Objects;
 public class Stream {
   private String streamName;
   private int replicationFactor;
+  private int backlogDuration;
 
-  public Stream(String streamName, int replicationFactor) {
+  public Stream(String streamName, int replicationFactor, int backlogDuration) {
     this.streamName = streamName;
     this.replicationFactor = replicationFactor;
+    this.backlogDuration = backlogDuration;
   }
 
   public String getStreamName() {
     return streamName;
   }
 
-  public int getReplicationFactor() {
-    return replicationFactor;
-  }
-
   public void setStreamName(String streamName) {
     this.streamName = streamName;
+  }
+
+  public int getReplicationFactor() {
+    return replicationFactor;
   }
 
   public void setReplicationFactor(int replicationFactor) {
     this.replicationFactor = replicationFactor;
   }
 
+  public int getBacklogDuration() {
+    return backlogDuration;
+  }
+
+  public void setBacklogDuration(int backlogDuration) {
+    this.backlogDuration = backlogDuration;
+  }
+
   @Override
   public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
     Stream stream = (Stream) o;
     return replicationFactor == stream.replicationFactor
-        && Objects.equals(streamName, stream.streamName);
+        && backlogDuration == stream.backlogDuration
+        && streamName.equals(stream.streamName);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(streamName, replicationFactor);
+    return Objects.hash(streamName, replicationFactor, backlogDuration);
   }
 }
