5b7a56fea992090c7eb3a89403dd6f75246d0e6d
s12f <97083380+s12f@users.noreply.github.com>
2022-03-22 15:50:36 +0800

    feat: batch acks (#90)

--- client/src/main/kotlin/io/hstream/impl/ResponderImpl.kt
+++ client/src/main/kotlin/io/hstream/impl/ResponderImpl.kt
@@ -4,7 +4,11 @@ import io.hstream.Responder
 import io.hstream.internal.RecordId
 import io.hstream.internal.StreamingFetchRequest
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import org.slf4j.LoggerFactory
+import kotlin.collections.ArrayList
 
 class ResponderImpl(
     private val subscriptionId: String,
@@ -13,15 +17,29 @@ class ResponderImpl(
     private val recordId: RecordId
 ) : Responder {
     override fun ack() {
-        val request = StreamingFetchRequest.newBuilder()
-            .setSubscriptionId(subscriptionId)
-            .setConsumerName(consumerId)
-            .addAckIds(recordId)
-            .build()
-        futureForIO { ackFlow.emit(request) }
+        runBlocking {
+            lock.withLock {
+                if (!buffer.containsKey(consumerId)) {
+                    buffer[consumerId] = ArrayList(bufferSize)
+                }
+                buffer[consumerId]!!.add(recordId)
+                if (buffer[consumerId]!!.size >= bufferSize) {
+                    val request = StreamingFetchRequest.newBuilder()
+                        .setSubscriptionId(subscriptionId)
+                        .setConsumerName(consumerId)
+                        .addAckIds(recordId)
+                        .build()
+                    ackFlow.emit(request)
+                    buffer.remove(consumerId)
+                }
+            }
+        }
     }
 
     companion object {
+        private const val bufferSize = 100
+        private val lock = Mutex()
+        private val buffer: HashMap<String, MutableList<RecordId>> = HashMap()
         private val logger = LoggerFactory.getLogger(ResponderImpl::class.java)
     }
 }
