5c1f263d8d0e0ef9cce50a0dda6748523e101f83
daleiz <30970925+daleiz@users.noreply.github.com>
2022-07-25 11:29:42 +0800

    update default key

--- client/src/main/java/io/hstream/impl/DefaultSettings.java
+++ client/src/main/java/io/hstream/impl/DefaultSettings.java
@@ -6,5 +6,5 @@ public class DefaultSettings {
 
   public static final int APPEND_RETRY_MAX_TIMES = 5;
 
-  public static final String DEFAULT_ORDERING_KEY = "__default__";
+  public static final String DEFAULT_ORDERING_KEY = "";
 }
