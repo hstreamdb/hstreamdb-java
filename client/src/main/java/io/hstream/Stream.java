84803c2f0aac775db367c505faebe01bb3d9c44a
s12f <97083380+s12f@users.noreply.github.com>
2022-10-19 12:10:40 +0800

    feat: add StreamBuilder and Replace Record with ReceivedRecord in Reader (#152)
    feat: add StreamBuilder and HStreamClient.createStream(Stream stream)

--- client/src/main/java/io/hstream/Stream.java
+++ client/src/main/java/io/hstream/Stream.java
@@ -1,5 +1,8 @@
 package io.hstream;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.util.Objects;
 
 public class Stream {
@@ -61,5 +64,60 @@ public class Stream {
   @Override
   public int hashCode() {
     return Objects.hash(streamName, replicationFactor, backlogDuration, shardCount);
+  }
+
+  public static final class Builder {
+    private String streamName;
+    private int replicationFactor = 1;
+    private int backlogDuration = 3600 * 24;
+    private int shardCount = 1;
+
+    /**
+     * @param streamName required, the name of the stream
+     * @return Stream.Builder instance
+     */
+    public Builder streamName(String streamName) {
+      this.streamName = streamName;
+      return this;
+    }
+
+    /**
+     * @param replicationFactor optional(default: 1), replication factor of the stream
+     * @return Stream.Builder instance
+     */
+    public Builder replicationFactor(int replicationFactor) {
+      this.replicationFactor = replicationFactor;
+      return this;
+    }
+
+    /**
+     * @param backlogDuration optional(default: 3600 * 24), backlog duration(in seconds) of the
+     *     stream
+     * @return Stream.Builder instance
+     */
+    public Builder backlogDuration(int backlogDuration) {
+      this.backlogDuration = backlogDuration;
+      return this;
+    }
+
+    /**
+     * @param shardCount optional(default: 1), number of shards in the stream
+     * @return Stream.Builder instance
+     */
+    public Builder shardCount(int shardCount) {
+      this.shardCount = shardCount;
+      return this;
+    }
+
+    public Stream build() {
+      checkNotNull(streamName);
+      checkArgument(replicationFactor >= 1 && replicationFactor <= 15);
+      checkArgument(shardCount >= 1);
+      return new Stream(streamName, replicationFactor, backlogDuration, shardCount);
+    }
+  }
+
+  public static Builder newBuilder() {
+    return new Builder();
   }
 }
