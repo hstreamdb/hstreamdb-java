c0d99e2d53b85ff8e0f594c12ea125ad6a7eaa40
daleiz <30970925+daleiz@users.noreply.github.com>
2023-04-18 10:45:04 +0800

    fix stub for streamingFetch

--- client/src/main/kotlin/io/hstream/impl/ConsumerKtImpl.kt
+++ client/src/main/kotlin/io/hstream/impl/ConsumerKtImpl.kt
@@ -71,7 +71,7 @@ class ConsumerKtImpl(
         if (!isRunning) return
         val server = lookupSubscription()
         logger.debug("lookupSubscription, received:[$server]")
-        val stub = client.getCoroutineStubWithTimeout(server)
+        val stub = client.getCoroutineStub(server)
         try {
             // send an empty ack request to trigger streamingFetch.
             val initRequest = StreamingFetchRequest.newBuilder()
